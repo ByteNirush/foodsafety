5d5976eb99e393f630700fe7452b816a5f45f520
ByteNirush <nirushstha@gmail.com>
2025-06-03 14:03:58 +0545

    dashboard

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.dashboard, name='core-dashboard'),
     path('login/', views.login, name='core-login'),
     path('signup/', views.signup, name='core-signup'),
+    path('community-watch/', views.community_watch, name='core-community-watch'),
+    path('ai-waste-dashboard/', views.ai_waste_dashboard, name='core-ai-waste-dashboard'),
+    path('ingredient-scanner-dashboard/', views.ingredient_scanner_dashboard, name='core-ingredient-scanner-dashboard'),
 ]
--- core/views.py
+++ core/views.py
@@ -6,4 +6,15 @@ def login(request):
 def signup(request):
     return render(request, 'core/signup.html')
 
+def dashboard(request):
+    return render(request, 'core/dashboard.html')
+
+def community_watch(request):
+    return render(request, 'core/community_watch.html')
+
+def ai_waste_dashboard(request):
+    return render(request, 'core/ai_waste_dashboard.html')
+
+def ingredient_scanner_dashboard(request):
+    return render(request, 'core/ingredient_scanner_dashboard.html')
 
